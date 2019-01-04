ca2a8f5959e7fc6b13311de90405136168e321f8
leahgwin <leahgwin@gmail.com>
2019-01-03 22:49:28 -0600

    finished battle of bands take a number function

--- main.js
+++ main.js
@@ -5,12 +5,30 @@
 for (let currentNumber = 1; currentNumber <= 100; currentNumber++) {
   // Divide the current number and check if the remainder is 0
   if (currentNumber % 5 === 0 && currentNumber % 7 === 0) {
-    console.log("FizzBuzz");
+    //console.log("FizzBuzz");
   } else if (currentNumber % 7 === 0) {
-    console.log("Buzz");
+    //console.log("Buzz");
   } else if (currentNumber % 5 === 0) {
-    console.log("Fizz");
+    //console.log("Fizz");
   } else {
-    console.log(currentNumber);
+    //console.log(currentNumber);
   }
 }
+
+// A local nightclub is having a Battle of the Bands night in a few months. They expect many bands to sign up because the grand prize is $20,000. To make things easier on the nightclub management, they want each band to be assigned a number so that they can easily keep track of the order in which the bands will perform.
+
+// Your job is to write a function that accepts any band name as an argument. The function will increment a global variable by one each time it is invoked, and return that number, and the band name concatenated together.
+
+let bandNumber = 0;
+let bandName = "";
+
+const takeNumber = function(bandName) {
+  bandNumber++;
+  return `${bandNumber}. ${bandName}`;
+};
+
+const scum = takeNumber("Galactic Scum");
+console.log(scum); // This should print "1. Galactic Scum" in the console
+
+const under = takeNumber("Underdogs");
+console.log(under); // This should print "2. Underdogs" in the console
