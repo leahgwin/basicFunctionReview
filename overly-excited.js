3b2f75dd167cf62cd5c5d5c1153aa1e603eca3c9
leahgwin <leahgwin@gmail.com>
2019-01-03 23:26:34 -0600

    added even more excitement

--- overly-excited.js
+++ overly-excited.js
@@ -27,9 +27,47 @@ function addExcitement(theWordArray) {
     // Concatenate the new word onto buildMeUp
     buildMeUp = buildMeUp + " " + theWordArray[i] + "!";
     // Print buildMeUp to the console
-    console.log(buildMeUp);
+    //console.log(buildMeUp);
   }
 }
 
 // Invoke the function and pass in the array
 addExcitement(sentence);
+
+//part two
+
+let sentence2 = [
+  "The",
+  "walrus",
+  "danced",
+  "through",
+  "the",
+  "trees",
+  "in",
+  "the",
+  "light",
+  "of",
+  "the",
+  "moon"
+];
+
+function addExcitement(theWordArray) {
+  let buildMeUp = "";
+
+  for (let i = 0; i < theWordArray.length; i++) {
+    // If the current value of `i` divided by 3 has no
+    // remainder, add an exclamation point to the end of
+    // the word and then concatenate it to `buildMeUp`.
+    if ([i] % 3 === 0) {
+      buildMeUp = buildMeUp + " " + theWordArray[i] + "!";
+    } else {
+      buildMeUp = buildMeUp + " " + theWordArray[i];
+    }
+    // Otherwise, just concatenate the word itself.
+
+    // Print buildMeUp to the console
+    console.log(buildMeUp);
+  }
+}
+
+addExcitement(sentence2);
